fb2d977a8f233ef50fd332ab3db6f82a91b59e1f
owlbbame <78080534+owlbbame@users.noreply.github.com>
2021-05-13 16:06:18 +0900

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,32 @@
+let painting = false;
+let filling = false;
+
+var canvas=document.getElementById("canvas");
+var ctx=canvas.getContext("2d");
+
+function startPainting() {
+  painting = true;
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
